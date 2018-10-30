9c3ba96b2f24474a1e17376ccb23b1f75cd97574
Joel Amálio <joel.amalio@gmail.com>
2018-10-30 18:51:33 -0300

    Usando o Default Methods.

---
+++ src/main/java/br/com/joelamalio/defaultmethods/ExemploColecao.java
@@ -0,0 +1,39 @@
+package br.com.joelamalio.defaultmethods;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ExemploColecao {
+	
+	public static void main(String[] args) {
+		List<String> lista = new ArrayList<String>();
+		
+		lista.add("Professor");
+		lista.add("Cantor");
+		lista.add("Cirugião");
+		lista.add("Programador");
+		lista.add("Diretor");
+		lista.add("Enfermeira");
+		
+		OrdenacaoPorTamanho ordenacaoPorTamanho = new OrdenacaoPorTamanho();
+		lista.sort(ordenacaoPorTamanho);
+		
+		ImprimirLinhaNoConsole imprimirLinhaNoConsole = new ImprimirLinhaNoConsole();
+		lista.forEach(imprimirLinhaNoConsole);
+	}
+
+}
+
+class OrdenacaoPorTamanho implements Comparator<String> {
+	public int compare(String s1, String s2) {
+		return Integer.compare(s1.length(), s2.length());
+	}
+}
+
+class ImprimirLinhaNoConsole implements Consumer<String> {
+	public void accept(String s) {
+		System.out.println(s);
+	}
+}
