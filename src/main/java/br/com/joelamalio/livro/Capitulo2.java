0e5e7a5153f892c76cdebbcb1b64d1646437a029
Joel Amálio <joel.amalio@gmail.com>
2018-12-13 11:11:30 -0300

    Exemplo de Lambda do livro.

---
+++ src/main/java/br/com/joelamalio/livro/Capitulo2.java
@@ -0,0 +1,25 @@
+package br.com.joelamalio.livro;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Capitulo2 {
+	
+	public static void main(String[] args) {
+		Usuario usuario1 = new Usuario("Pedro", 140);
+		Usuario usuario2 = new Usuario("Roberta", 120);
+		Usuario usuario3 = new Usuario("Gabriel", 180);
+		
+		List<Usuario> usuarios = Arrays.asList(usuario1, usuario2, usuario3);
+		
+		System.out.println("Execução do lambda com variável temporária:");
+		Consumer<Usuario> mostrador = u -> System.out.println(u.getNome());
+		usuarios.forEach(mostrador);
+		
+		System.out.println("Execução direta do lambda:");
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+		
+	}
+
+}
